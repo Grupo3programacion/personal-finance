5e979134e1da8db17bb371faff69cb4ffe0872c6
cristiandecker <cristianc.cortesb@uqvirtual.edu.co>
2026-01-09 16:32:09 -0500

    correcciones v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/finanzaApp.png",
         media: "(prefers-color-scheme: light)",
       },
       {
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/finanzaApp.png",
   },
 }
 
